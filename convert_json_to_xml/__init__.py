308303ac7aa48656540d2ec3fd36328ab782a503
Marco Pulido <marcopulido00@gmail.com>
2024-07-31 16:39:42 +0800

    async upload now working

--- convert_json_to_xml/__init__.py
+++ convert_json_to_xml/__init__.py
@@ -7,51 +7,68 @@ import tempfile
 
 import azure.functions as func
 
+# from azure.storage.blob import BlobClient, BlobServiceClient, ContainerClient
+from azure.storage.blob.aio import BlobClient, BlobServiceClient, ContainerClient
+
 from ..json_schemas import custom_transforms_dict
-from ..storage_utils import get_file, upload_files_to_blob_storage
+from ..storage_utils import get_file
 from ..xml_transform_utils import XMLTransformUtils
 
+# async def upload_blob_file(self, blob_service_client: BlobServiceClient, container_name: str):
+#     container_client = blob_service_client.get_container_client(container=container_name)
+#     with open(file=os.path.join('filepath', 'filename'), mode="rb") as data:
+#         blob_client = await container_client.upload_blob(name="sample-blob.txt", data=data, overwrite=True)
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    try:
-        logging.info("Python HTTP trigger function processed a request.")
+# async def async_main(blob_data):
+#     # TODO: Replace <storage-account-name> with your actual storage account name
+#     account_url = "https://testrbt109238102983.blob.core.windows.net"
+#     connection_string = os.getenv("AzureStorageConnectionString")
 
-        xml_outputs = []
+#     async with BlobServiceClient.from_connection_string(connection_string) as blob_service_client:
+#         await blob_data.upload_blob_file(blob_service_client, "xmlexport")
 
-        xmlTransformUtils = XMLTransformUtils()
-        input_json_file = get_file(
-            filename=req.params["filename"], filepath=req.params["filepath"]
-        ).decode("utf-8")
 
-        asyncio.run(
-            upload_files_to_blob_storage(
-                [x for x in input_json_file.strip().split("\n")]
-            )
+async def convert_and_process_file(input_json_str: str):
+    # Convert file
+    xmlTransformUtils = XMLTransformUtils()
+    input_json = json.loads(input_json_str)
+    xml_output = XMLTransformUtils().json_to_xml(
+        json_obj=input_json,
+        dict_type="Ingredient",  # TODO infer from schema
+        root_name="FormulationML",
+        custom_transforms_dict=custom_transforms_dict,
+    )
+
+    connection_string = os.getenv("AzureStorageConnectionString")
+    container_name = "xmlexport"
+    async with BlobServiceClient.from_connection_string(
+        connection_string
+    ) as blob_service_client:
+        container_client = blob_service_client.get_container_client(container_name)
+        blob_name = os.path.join(
+            "output_ingredient_xml",
+            "20240716",  # TODO get somewhere else; maybe another function param
+            f"output_{input_json['IngredientCode']}.xml",
         )
-    except Exception as e:
-        return func.HttpResponse(f"Error: {e}", status_code=400)
+        blob_client = container_client.get_blob_client(blob_name)
 
-    batch = []
-    # for input_json in [x for x in input_json_file.strip().split("\n")]:
-    #     input_json = json.loads(input_json)
-    #     xml_output = xmlTransformUtils.json_to_xml(
-    #         json_obj=input_json,
-    #         dict_type=req.params["type"],
-    #         root_name="FormulationML",
-    #         custom_transforms_dict=custom_transforms_dict,
-    #     )
+        await blob_client.upload_blob(xml_output, overwrite=True)
+        print(f"Uploaded to blob {blob_client.blob_name}.")
 
-    #     # Create a temporary file
-    #     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-    #         # Write the string content to the temporary file
-    #         tmp_file.write(xml_output.encode())
-    #         # Get the path of the temporary file
-    #         tmp_file_path = tmp_file.name
 
-    #     batch.append(
-    #         tmp_file_path,
-    #     )
-    # return func.HttpResponse(f"Error: {e}", status_code=400)
+async def main(req: func.HttpRequest) -> func.HttpResponse:
+    # try:
+    logging.info("Python HTTP trigger function processed a request.")
+
+    xml_outputs = []
+
+    input_json_file = get_file(
+        filename=req.params["filename"], filepath=req.params["filepath"]
+    ).decode("utf-8")
+
+    input_jsons = [x for x in input_json_file.strip().split("\n")]
+    tasks = [asyncio.create_task(convert_and_process_file(x)) for x in input_jsons]
+    done, pending = await asyncio.wait(tasks)
 
     return func.HttpResponse(
         json.dumps(xml_outputs),
--- storage_utils.py
+++ storage_utils.py
@@ -29,63 +29,47 @@ def get_file(filename: str, filepath: str):
     return blob_data
 
 
-# def upload_file(filename: str, filepath: str, data):
-#     connect_str = os.getenv("AzureStorageConnectionString")
-#     container_name = "xmlexport"
-#     blob_name = os.path.join(filepath, filename)
-
-#     # Create the BlobServiceClient object
-#     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-
-#     # Get the blob client
-#     blob_client = blob_service_client.get_blob_client(
-#         container=container_name, blob=blob_name
-#     )
-
-#     blob_client.upload_blob(data, overwrite=True)
-
-
-async def upload_file(blob_name, data):
-    try:
-        connection_string = os.getenv("AzureStorageConnectionString")
-        container_name = "xmlexport"
-        blob_service_client = BlobServiceClient.from_connection_string(
-            connection_string
-        )
-        container_client = blob_service_client.get_container_client(container_name)
-        blob_client = container_client.get_blob_client(blob_name)
-        async with blob_client as bc:
-            await bc.upload_blob(data, overwrite=True)
-            print(f"Uploaded to blob {blob_client.blob_name}.")
-    except Exception as e:
-        print(f"Could not upload file: {e}")
+# async def upload_file(blob_name, data):
+#     try:
+#         connection_string = os.getenv("AzureStorageConnectionString")
+#         container_name = "xmlexport"
+#         blob_service_client = BlobServiceClient.from_connection_string(
+#             connection_string
+#         )
+#         container_client = blob_service_client.get_container_client(container_name)
+#         blob_client = container_client.get_blob_client(blob_name)
+#         async with blob_client as bc:
+#             await bc.upload_blob(data, overwrite=True)
+#             print(f"Uploaded to blob {blob_client.blob_name}.")
+#     except Exception as e:
+#         print(f"Could not upload file: {e}")
 
 
-async def upload_files_to_blob_storage(input_jsons):
-    try:
-        # Create tasks for concurrent uploading
-        tasks = []
-        for input_json in input_jsons:
-            input_json = json.loads(input_json)
-            xml_output = XMLTransformUtils().json_to_xml(
-                json_obj=input_json,
-                dict_type="Ingredient",  # TODO infer from schema
-                root_name="FormulationML",
-                custom_transforms_dict=custom_transforms_dict,
-            )
-            blob_name = os.path.join(
-                "output_ingredient",
-                "20240716",  # TODO get somewhere else; maybe another function param
-                f"output_{input_json['IngredientCode']}",
-            )
-            print(blob_name)
-            tasks.append(upload_file(blob_name, xml_output))
+# async def upload_files_to_blob_storage(input_jsons):
+#     try:
+#         # Create tasks for concurrent uploading
+#         tasks = []
+#         for input_json in input_jsons:
+#             input_json = json.loads(input_json)
+#             xml_output = XMLTransformUtils().json_to_xml(
+#                 json_obj=input_json,
+#                 dict_type="Ingredient",  # TODO infer from schema
+#                 root_name="FormulationML",
+#                 custom_transforms_dict=custom_transforms_dict,
+#             )
+#             blob_name = os.path.join(
+#                 "output_ingredient",
+#                 "20240716",  # TODO get somewhere else; maybe another function param
+#                 f"output_{input_json['IngredientCode']}",
+#             )
+#             print(blob_name)
+#             tasks.append(upload_file(blob_name, xml_output))
 
-        print("start tasks")
-        # Run tasks concurrently
-        await asyncio.gather(*tasks)
+#         print("start tasks")
+#         # Run tasks concurrently
+#         await asyncio.gather(*tasks)
 
-        print("All files uploaded successfully.")
+#         print("All files uploaded successfully.")
 
-    except Exception as e:
-        print(f"Could not upload files to blob: {e}")
+#     except Exception as e:
+#         print(f"Could not upload files to blob: {e}")
